f69fd4238fb649a5625c7ee429674faba9018028
Yoshiaki Teshima <38271745+Githubtesi@users.noreply.github.com>
2026-03-27 10:41:53 +0900

    Add AggregateRoot and exceptions to domain module

--- src/seedwork/domain/__init__.py
+++ src/seedwork/domain/__init__.py
@@ -1,3 +1,9 @@
 from .value_object import ValueObject
 from .entity import Entity
+from .aggregate_root import AggregateRoot
 from .repository import IRepository
+from .exceptions import (
+    DomainException, 
+    ValueObjectValidationError, 
+    EntityNotFoundError
+)
